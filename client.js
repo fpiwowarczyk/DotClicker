ce932880b52b89055de0e7e561cf9467ed93618e
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2020-06-15 11:33:14 +0200

    Closer to end the project

--- client.js
+++ client.js
@@ -1,92 +1,121 @@
 var wsUri = "ws://localhost:8888/ws";
-var wsUriecho="ws://echo.websocket.org";
 var output;
-var input;
-var X,Y,len;
+var canvas,ctx;
+var X,Y;
+var messageCounter=0;
+var rectWidth=150,rectHeight=75;
+var gX=150,gY=75,gLen=0;
 
+var colors =['blue','green','yellow','magenta','red']
 
-
-
-
-
-var msg = {
-  type: "message",
-  text: "Hello world",
-  id: 101,
-  date: Date.now()
-};
-
+ // Game init
 function init()
 {
   output = document.getElementById("output");
-  testWebSocket();
-  if(window.Event){
-    document.captureEvents(Event.MOUSEMOVE);
-  }
-  document.onmousedown=getCursorXY
+  game();
+  initCanvas();
 
 }
 
-function getCursorXY(e){
-  X=document.getElementById('cursorX').value=(window.Event)?e.pageX:event.clientX+(document.documentElement.scrollLeft? document.documentElement.scrollLeft:document.bodyscrollLeft);
-  Y=document.getElementById('cursorY').value=(window.Event)?e.pageY:event.clientY+(document.documentElement.strollTop?document.documentElement.scrollTop:document.body.scrollTop);
-  sendCoords(X,Y);
-}
-
-function testWebSocket()
+function game()
 {
   websocket = new WebSocket(wsUri);
   websocket.onopen = function(e) { onOpen(e) };
   websocket.onclose = function(e) { onClose(e) };
   websocket.onmessage = function(e) { onMessage(e) };
   websocket.onerror = function(e) { onError(e) };
+
+  document.onmousedown=getCursorXY;
+  
 }
-function sendMessage()
-{
-  input =document.getElementById("message").value;
-  doSend(input);
-}
+ // Base functions ===========
 function onOpen(e)
 {
   console.log(e.type);
   writeToScreen("CONNECTED");
 }
 
+
 function onClose(e)
 {
   writeToScreen("DISCONNECTED");
 }
 
+
 function onMessage(e)
 {
-  var msg = new ArrayBuffer(2);
-  var msgView = new DataView(msg);
-  msgView.getInt16(0,e.data);
-  console.log(msg);
-  writeToScreen('<span style="color: blue;">RESPONSE: ' + /*JSON.parase(e.data)*/msg[0]+'</span>');
+  console.log(e);
+  if (e.data instanceof ArrayBuffer)
+  { 
+    messageCounter=messageCounter+1;
+    document.getElementById("message counter").value=messageCounter;
+    view = new Int16Array(e.data);
+    gX=view[0];
+    gY=view[1];
+    view= new Float32Array(e.data);
+    gLen=view[1];
+    circleDraw(gX,gY,colors[gX%5]);
+  } else if (!(e.data instanceof Blob)){
+    writeToScreen("PLAYER "+e.data);
+  }
 }
+
 
 function onError(e)
 {
   writeToScreen('<span style="color: red;">ERROR:</span> ' + e.data);
 }
 
-function doSend(message)
-{
-  writeToScreen("Text message: " + message);
-  websocket.send(message);
+//=====================================
+
+function initCanvas(){
+  canvas= document.getElementById("canvas");
+  canvas.width = window.innerWidth-50;
+  canvas.height = window.innerHeight;
+  console.log(canvas.width);
+  console.log(canvas.height);
+  ctx=canvas.getContext("2d");
+}
+function rectDraw(pX,pY,nX,nY){
+  ctx.fillStyle=colors["white"];
+  ctx.fillRect(pX-110,pY-240,rectWidth,rectHeight+20);
+  
+  ctx.fillStyle=colors["red"];
+  ctx.fillRect(nX-110,nY-240,rectWidth,rectHeight);
+}
+
+function circleDraw(X,Y,color){
+  ctx.beginPath();
+  ctx.arc(X,Y,50,0,2*Math.PI);
+  ctx.strokeStyle = color;
+  ctx.stroke();
+
+}
+
+function getCursorXY(e){
+  X=(window.Event)?e.pageX:event.clientX+(document.documentElement.scrollLeft? document.documentElement.scrollLeft:document.bodyscrollLeft);
+  Y=(window.Event)?e.pageY:event.clientY+(document.documentElement.strollTop?document.documentElement.scrollTop:document.body.scrollTop);
+  console.log(X);
+  console.log(Y);
+  sendCoords(X,Y);
 }
 
 function sendCoords(X,Y){
   websocket.binaryType="arraybuffer";
   var buffer = new ArrayBuffer(8);
   var bufferView = new DataView(buffer);
-  bufferView.setInt16(1,X);
-  bufferView.setInt16(5,Y)
+  bufferView.setInt16(1,canvas.width);
+  bufferView.setInt16(5,canvas.height);
   websocket.send(buffer);
 }
-
-
+/*function makeMessage(Player,X,Y){
+  websocket.binaryType="arraybuffer";
+  var buffer = new ArrayBuffer(12);
+  var bufferView = new DataView(buffer);
+  bufferView.setInt16(1,Player);
+  bufferView.setInt16(5,X);
+  bufferView.setInt16(9,Y);
+}*/
 function writeToScreen(message)
 {
   var pre = document.createElement("p");
@@ -94,5 +123,4 @@ function writeToScreen(message)
   pre.innerHTML = message;
   output.appendChild(pre);
 }
-
 window.addEventListener("load", init, false);
--- game-server.py
+++ game-server.py
@@ -3,26 +3,52 @@ import tornado.websocket
 import tornado.ioloop
 import struct 
 import math 
+import random
 
-
+n=1
+state = {"X":0,"Y":0,"Len":0}
 class GameHandler(tornado.websocket.WebSocketHandler):
 
+    connections = set()
+    
     def open(self):
-        print("WebSocket open")
+        global n,state
+        self.connections.add(self)
+        self.write_message(str(n))
+        n=n+1
+        msg=struct.pack('hhf',state.get("X"),
+                        state.get("Y"),
+                        state.get("Len"))
+        self.write_message(msg,True)
+        print("New Connection")
+        
+        
 
     def on_close(self):
-        print("WebSocket Close")
+        global n
+        self.connections.remove(self)
+        n=n-1
+        print("Connection Removed")
+        
     
     def on_message(self,message):
+        global state
+        #print(state)
         if isinstance(message,bytes):
+            #Get msg and countLength from 0,0
             message=struct.unpack('hhhh',message)
-            X=message[0]+message[1]
-            Y=message[2]+message[3]
-            Len=countLen(X,Y)
-            print("X="+str(X)+"\nY="+str(Y)+"\nLen="+str(Len))
-            msg=struct.pack('hhf',X,Y,Len)
-            print(msg)
-            self.write_message(msg,True)
+            state["X"]=message[0]+message[1]
+            state["Y"]=message[2]+message[3]
+            state["Len"]=countLen(state.get("X"),state.get("Y"))
+            # Send msg 
+            for i in range(0,1000):
+                x=random.randrange(state["X"])
+                y=random.randrange(state["Y"])
+                msg=struct.pack('hhf',
+                                x,
+                                y,
+                                state.get("Len"))
+                [con.write_message(msg,True) for con in self.connections]
         else:
             print("Get Message:")
             self.write_message(message)
@@ -30,20 +56,23 @@ class GameHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self,origin):
         print("Origin: ",origin)
         return True
-    
-class Loader(tornado.web.RequestHandler):
-    def get(self):
-        self.render("websocket.html",title="Dotes Clicker")
+
 
 def countLen(X,Y):
     Len=math.sqrt(X*X+Y*Y)
     return Len
         
+
+class Loader(tornado.web.RequestHandler):
+    def get(self):
+        self.render("websocket.html",title="Dotes Clicker")
+
+
 if __name__ == "__main__":
     app = tornado.web.Application([
         ("/",Loader),
         ("/ws",GameHandler),
-        ("static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
+        ("/(.*)", tornado.web.StaticFileHandler, {"path": ""}),
     ])
     app.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
